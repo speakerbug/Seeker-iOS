e73bd7f9e07d2134a902d34f4de6b4f23cfbcf75
Henry Saniuk <henry@henrysaniuk.com>
2015-07-29 10:49:42 -0500

    Added auth manager for tracking if user is logged in

---
+++ Seeker/AuthenticationManager.swift
@@ -0,0 +1,42 @@
+//
+//  AuthenticationManager.swift
+//  Seeker
+//
+//  Created by Henry Saniuk on 7/29/15.
+//  Copyright (c) 2015 Henry Saniuk. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class AuthenticationManager: NSObject {
+    
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    private struct Constants {
+        static let sharedManager = AuthenticationManager()
+    }
+    
+    class var sharedManager: AuthenticationManager {
+        return Constants.sharedManager
+    }
+    
+    
+    var userID: Int {
+        get {
+            return defaults.integerForKey("userID")
+        } set(newID) {
+            defaults.setInteger(newID, forKey: "userID")
+        }
+    }
+    
+    func logoutUser() {
+        self.userID = 0
+    }
+    
+    var userIsLoggedIn: Bool {
+        return self.userID != 0
+    }
+    
+}
