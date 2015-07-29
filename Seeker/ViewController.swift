575f18554925582942260b5b17bf05df8a094917
Henry Saniuk <henry@henrysaniuk.com>
2015-07-29 14:24:40 -0500

    Added map and current location

--- Seeker/ViewController.swift
+++ Seeker/ViewController.swift
@@ -8,26 +8,101 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    let locManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let initialLocation = CLLocation(
+        
+        // Core Location Manager asks for GPS location
+        locManager.delegate = self
+        locManager.desiredAccuracy = kCLLocationAccuracyBest
+        locManager.requestWhenInUseAuthorization()
+        locManager.startMonitoringSignificantLocationChanges()
+        
+        // Check if the user allowed authorization
+        if   (CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() == CLAuthorizationStatus.AuthorizedAlways)
+        {
+            
+            println(locManager.location)
+            
+        }  else {
+            println("Not authorized")
+        }
+        
+        let location = "1 Infinity Loop, Cupertino, CA"
+        var geocoder:CLGeocoder = CLGeocoder()
+        geocoder.geocodeAddressString(location, completionHandler: {(placemarks, error) -> Void in
+            
+            if((error) != nil){
+                
+                println("Error", error)
+            }
+                
+            else if let placemark = placemarks?[0] as? CLPlacemark {
+                
+                var placemark:CLPlacemark = placemarks[0] as! CLPlacemark
+                var coordinates:CLLocationCoordinate2D = placemark.location.coordinate
+                
+                var pointAnnotation:MKPointAnnotation = MKPointAnnotation()
+                pointAnnotation.coordinate = coordinates
+                pointAnnotation.title = "Apple HQ"
+                
+                self.mapView?.addAnnotation(pointAnnotation)
+                self.mapView?.centerCoordinate = coordinates
+                self.mapView?.selectAnnotation(pointAnnotation, animated: true)
+                
+                println("Added annotation to map view")
+            }
+            
+        })
+    }
+    
+    func mapView (mapView: MKMapView!,
+        viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+            
+            var pinView:MKPinAnnotationView = MKPinAnnotationView()
+            pinView.annotation = annotation
+            pinView.pinColor = MKPinAnnotationColor.Red
+            pinView.animatesDrop = true
+            pinView.canShowCallout = true
+            
+            return pinView
+    }
+    
+    func mapView(mapView: MKMapView!,
+        didSelectAnnotationView view: MKAnnotationView!){
+            
+            println("Selected annotation")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        locManager.stopUpdatingLocation()
+        if ((error) != nil) {
+            print(error)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var locationArray = locations as NSArray
+        var locationObj = locationArray.lastObject as! CLLocation
+        var coord = locationObj.coordinate
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     override func viewDidAppear(animated: Bool) {
         if !AuthenticationManager.sharedManager.userIsLoggedIn {
             self.performSegueWithIdentifier("showLogin", sender: self)
         }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 }
